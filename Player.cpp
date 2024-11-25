57262b1b570b0dc251bc4bba3ed92c621d59e991
Julian Daneman <danemanj@mcmaster.ca>
2024-11-24 23:40:52 -0500

    Finished Iteration 1A

--- Player.cpp
+++ Player.cpp
@@ -7,7 +7,9 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
-    
+    playerPos.pos->x = 5; //starting horizontal player position
+    playerPos.pos->y = 5; //starting vertical player position
+    playerPos.symbol = '*'; //player avatar symbol
 }
 
 
@@ -24,12 +26,94 @@ objPos Player::getPlayerPos() const
 
 void Player::updatePlayerDir()
 {
-        // PPA3 input processing logic          
+        // PPA3 input processing logic  
+        char input;
+        input = mainGameMechsRef->getInput(); //retrieve current user input
+        
+        if(input != 0)  // if not null character
+        {
+            switch(input) // carry out actions depending on user input
+            {                      
+                case 27:  // exit
+                    mainGameMechsRef->setExitTrue();
+                    break;
+
+                case 'w': //up
+                    if(myDir == STOP || myDir == LEFT || myDir == RIGHT)
+                        myDir = UP;
+                    break;
+                
+                case 's': //down
+                    if(myDir == STOP || myDir == LEFT || myDir == RIGHT)
+                        myDir = DOWN;
+                    break;
+
+                case 'a': //left
+                    if(myDir == STOP || myDir == UP || myDir == DOWN)
+                        myDir = LEFT;
+                    break;
+
+                case 'd': //right
+                    if(myDir == STOP || myDir == UP || myDir == DOWN)
+                        myDir = RIGHT;
+                    break;
+
+                default:
+                    break;
+            }
+                    
+            mainGameMechsRef->clearInput(); //clear input so it doesnt get used multiple times
+        }         
 }
 
 void Player::movePlayer()
 {
+    int bSizeX = mainGameMechsRef->getBoardSizeX();
+    int bSizeY = mainGameMechsRef->getBoardSizeY();
+    
     // PPA3 Finite State Machine logic
+    // update the player location by 1 unit in the direction stored in the program
+    switch(myDir)
+    {
+        case UP:
+            playerPos.pos->y--;
+            break;
+        
+        case DOWN:
+            playerPos.pos->y++;
+            break;
+        
+        case LEFT:
+            playerPos.pos->x--;
+            break;
+
+        case RIGHT:
+            playerPos.pos->x++;
+            break;
+        
+        default:
+            break;
+    }
+
+    //wraparound if past edge of screen
+    //vertically
+    if(playerPos.pos->y>=bSizeY-1)
+    {
+        playerPos.pos->y=1;
+    }
+    else if(playerPos.pos->y<=0)
+    {
+        playerPos.pos->y=bSizeY-2;
+    }
+    //horizontally
+    if(playerPos.pos->x>=bSizeX-1)
+    {
+        playerPos.pos->x=1;
+    }
+    else if(playerPos.pos->x<=0)
+    {
+        playerPos.pos->x=bSizeX-2;
+    }
 }
 
 // More methods to be added
--- Project.cpp
+++ Project.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "Player.h"
 
 using namespace std;
 
@@ -17,11 +18,14 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
-//arbitrary objPos testers (OBJPOS TESTER, DELETE LATER)
+//arbitrary objPos testers (TESTER, DELETE LATER)
 objPos arb1;
 objPos arb2;
 objPos arb3;
 
+//Player class test
+Player* player;
+GameMechs* game;
 
 int main(void)
 {
@@ -52,6 +56,10 @@ void Initialize(void)
     arb1 = objPos(4,5,'A');
     arb2 = objPos(8,7,'B');
     arb3 = objPos(2,6,'C');
+
+    //Player class test
+    game = new GameMechs();
+    player = new Player(game);
 }
 
 void GetInput(void)
@@ -62,15 +70,22 @@ void GetInput(void)
 void RunLogic(void)
 {
     
+    //update and move player object
+    player->updatePlayerDir();
+    player->movePlayer();
 }
 
 void DrawScreen(void)
 {
     MacUILib_clearScreen();    
 
-
     int i;
     int j;
+    
+    //retrieve player position
+    int x = player->getPlayerPos().pos->x;
+    int y = player->getPlayerPos().pos->y;
+
     //iterate through game board, printing characters in grid
     for(i=0;i<BOARDSIZEY;i++)
     {
@@ -80,17 +95,21 @@ void DrawScreen(void)
             {
                 MacUILib_printf("#");
             } 
-            else if(j== arb1.pos->y && i==arb1.pos->x) //print arbitrary symbols (OBJPOS TESTER, DELETE LATER)
+            else if(j== arb1.pos->x && i==arb1.pos->y) //print arbitrary symbols (OBJPOS TESTER, DELETE LATER)
             {
                 MacUILib_printf("%c",arb1.symbol);
             }
-            else if(j== arb2.pos->y && i==arb2.pos->x) //print arbitrary symbols (OBJPOS TESTER, DELETE LATER)
+            else if(j== arb2.pos->x && i==arb2.pos->y) //print arbitrary symbols (OBJPOS TESTER, DELETE LATER)
             {
                 MacUILib_printf("%c",arb2.symbol);
             }
-            else if(j== arb3.pos->y && i==arb3.pos->x) //print arbitrary symbols (OBJPOS TESTER, DELETE LATER)
+            else if(j== arb3.pos->x && i==arb3.pos->y) //print arbitrary symbols (OBJPOS TESTER, DELETE LATER)
             {
                 MacUILib_printf("%c",arb3.symbol);
+            }
+            else if(j == x && i == y) //print arbitrary symbols (OBJPOS TESTER, DELETE LATER)
+            {
+                MacUILib_printf("%c",player->getPlayerPos().symbol);
             }
             else //print empty space
             {
